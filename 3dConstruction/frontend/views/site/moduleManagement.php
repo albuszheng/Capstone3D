d61b3453d3202b08f9baf69e1bbe3ed583798c32
Albus Zheng <albuszheng@hotmail.com>
2016-05-25 15:57:05 +0800

    problem with template loading

--- 3dConstruction/frontend/views/site/moduleManagement.php
+++ 3dConstruction/frontend/views/site/moduleManagement.php
@@ -9,14 +9,12 @@ $this->title = 'Manage Module';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-manage-module">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>模板管理</p>
 
     <div class="modal fade template-canvas" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
         <div class="modal-dialog modal-lg">
-            <div class="modal-content canvas">
-                //put your canvas in here
+            <div class="modal-title"></div>
+            <div class="modal-content canvas" id="module-canvas">
+
             </div>
         </div>
     </div>
@@ -37,9 +35,8 @@ $this->params['breadcrumbs'][] = $this->title;
                 foreach ($modules as $module): ?>
                     <tr>
                         <td><?= $module->id?></td>
-                        <td><button class="btn btn-link template-name" id=<?= $module->id?> data-toggle="modal" data-target=".template-canvas"><?= $module->name?></button></td>
+                        <td><button class="btn btn-link template-name" id=<?= $module->id?> data-toggle="modal" data-target=".template-canvas" data-content="<?= $module->data?>" data-name="<?= $module->name?>"><?= $module->name?></button></td>
                         <td><?= $module->size?></td>
-<!--                        <td>--><?//= $module->data?><!--</td>-->
                     </tr>
                 <?php endforeach;?>
 
@@ -49,3 +46,52 @@ $this->params['breadcrumbs'][] = $this->title;
     </div>
 
 </div>
+
+<script type="text/javascript" src="js/pixi.js"></script>
+<script type="text/javascript" src="js/three.js"></script>
+<script type="text/javascript" src="js/ThreeBSP.js"></script>
+<script type="text/javascript" src="js/FirstPersonControls.js"></script>
+<script type="text/javascript" src="js/ColladaLoader.js"></script>
+<script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="js/SceneExport.js"></script>
+<script type="text/javascript" src="js/SceneLoad.js"></script>
+
+<script>
+    $(".template-name").on('show.bs.modal', function (event) {
+
+
+        var pos = document.getElementById("pos");
+        var rot = document.getElementById("rot");
+        var models = [];
+
+        $.ajax({
+            type:'post',
+            data:{},
+            url:'<?php echo Yii::$app->getUrlManager()->createUrl('/site/find-all-models') ?>',
+            async : false,
+            success:function(data) {
+                models = data.models;
+            },
+
+            error:function(xhr) {
+                console.log(xhr.responseText);
+            }
+
+        });
+
+        var stage, floor, walls, group;
+
+        var button = $(event.relatedTarget);
+        var data = button.data('content');
+
+        var width2d = $('#module-canvas').width();
+        var height2d = $('#module-canvas').height();
+
+        var loader = new SceneLoad();
+        stage = loader.load2d(data, width2d, height2d, document.getElementById('#module-canvas'), models);
+        updateInfo();
+
+        var modal = $(this);
+        modal.find('.modal-title').text(button.data('name'));
+    })
+</script>
